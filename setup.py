8f6ac4235f32fc6cd23e0aae032016d9a4c22fb5
O <oaref92@hotmail.com>
2024-02-04 17:23:24 +0000

    chgs

--- setup.py
+++ setup.py
@@ -1,10 +1,22 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='ai_stream_interact',
-    version='0.1.0',
+    name="ai_stream_interact",
+    version='0.0.1,
     author='Omar Aref',
     author_email='oa_dev_acc_92@hotmail.com',
+    description='An model agnostic extensible package that allows for AI & LLM interactions on a video stream',
+    long_description_content_type="text/markdown",
     packages=find_packages(),
-    python_requires='>=3.9'
+    # install_requires=['opencv-python', 'pyautogui', 'pyaudio'],
+    keywords=['python', 'ai', 'llm', 'artificial intelligence', 'large language models', 'nlp', 'natural language processing', 'video', 'video stream'],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Topic :: Scientific/Engineering :: Image Recognition",
+        "Topic :: Scientific/Engineering :: Human Machine Interfaces",
+    ]
 )
